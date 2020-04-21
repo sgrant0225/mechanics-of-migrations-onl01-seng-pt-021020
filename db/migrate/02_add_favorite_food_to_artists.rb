b6135e16f28111951ccc96ee17401a565532ae59
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-21 04:07:20 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -3,3 +3,5 @@ class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
     add_column :artists, :favorite_food, :string
   end
 end
+
+#here we just told Active Record to add a column to the artists table called favorite_food and it will be a string
